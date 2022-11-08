790550faa00fae45a2dd9c2d10a2f3db67aa8237
Ana <92342881+anadognini@users.noreply.github.com>
2022-11-08 20:01:03 -0300

    Update SELECT_multiplas_tabelas.sql

--- BD/SELECT_multiplas_tabelas.sql
+++ BD/SELECT_multiplas_tabelas.sql
@@ -70,3 +70,22 @@ FROM veiculo v JOIN proprietario p
 	       JOIN localidade l 
                ON (p.cd_localidade = l.cd_localidade)
 WHERE p.sg_uf = 'SC';
+
+-- f)
+
+SELECT v.nr_placa, ma.ds_marca, mo.ds_modelo, v.nr_ano_mod, c.ds_combustivel 
+FROM veiculo v, marca ma, modelo mo, veiculo_combustivel vc, combustivel c
+WHERE v.cd_modelo = mo.cd_modelo 
+	AND mo.cd_marca = ma.cd_marca 
+	AND v.nr_placa = vc.nr_placa 
+	AND vc.cd_combustivel = c.cd_combustivel;
+	
+-- g)
+
+SELECT v.nr_placa, ma.ds_marca, mo.ds_modelo 
+FROM veiculo v, marca ma, modelo mo, veiculo_acessorio va, acessorio a 
+WHERE v.cd_modelo = mo.cd_modelo 
+	AND mo.cd_marca = ma.cd_marca 
+    	AND v.nr_placa = va.nr_placa 
+    	AND va.cd_acessorio = a.cd_acessorio
+    	AND a.ds_acessorio LIKE 'Direção hid%';
