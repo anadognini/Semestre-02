32260567428e2270e4f52b9ec0e42633a0ab9c51
Ana <92342881+anadognini@users.noreply.github.com>
2022-11-08 19:36:41 -0300

    Update SELECT_multiplas_tabelas.sql

--- BD/SELECT_multiplas_tabelas.sql
+++ BD/SELECT_multiplas_tabelas.sql
@@ -8,7 +8,7 @@ WHERE p.cd_localidade = l.cd_localidade;
 
 SELECT p.nm_proprietario, l.nm_localidade
 FROM proprietario p JOIN localidade l
-	ON (p.cd_localidade = l.cd_localidade);
+ON (p.cd_localidade = l.cd_localidade);
     
 -- b)
 
@@ -21,7 +21,8 @@ ORDER BY mo.ds_modelo;
 -- Ou
 
 SELECT mo.ds_modelo, ma.ds_marca AS "Apenas para conferência"
-FROM modelo mo JOIN marca ma ON (ma.cd_marca = mo.cd_marca)
+FROM modelo mo JOIN marca ma
+ON (ma.cd_marca = mo.cd_marca)
 WHERE ma.ds_marca = 'FIAT'
 ORDER BY mo.ds_modelo;
 
@@ -35,10 +36,11 @@ ORDER BY marca, modelo;
 -- Ou
 
 SELECT ma.ds_marca AS marca, mo.ds_modelo AS modelo
-FROM modelo mo JOIN marca ma ON (ma.cd_marca = mo.cd_marca)
+FROM modelo mo JOIN marca ma 
+ON (ma.cd_marca = mo.cd_marca)
 ORDER BY marca, modelo;
 
--- d) 
+-- d)
 
 SELECT v.nr_placa, v.nr_ano_mod, c.ds_cor
 FROM veiculo v, cor c
@@ -48,5 +50,23 @@ ORDER BY 2, 3;
 -- Ou
 
 SELECT v.nr_placa, v.nr_ano_mod, c.ds_cor
-FROM veiculo v JOIN cor c ON (v.cd_cor = c.cd_cor)
+FROM veiculo v JOIN cor c 
+ON (v.cd_cor = c.cd_cor)
 ORDER BY 2, 3;
+
+-- e)
+
+SELECT v.nr_placa, p.nm_proprietario, l.nm_localidade
+FROM veiculo v, proprietario p, localidade l
+WHERE v.cd_proprietario = p.cd_proprietario
+	AND p.cd_localidade = l.cd_localidade
+    AND p.sg_uf = 'SC';
+    
+-- Ou
+
+SELECT v.nr_placa, p.nm_proprietario, l.nm_localidade
+FROM veiculo v JOIN proprietario p 
+	       ON (v.cd_proprietario = p.cd_proprietario)
+	       JOIN localidade l 
+               ON (p.cd_localidade = l.cd_localidade)
+WHERE p.sg_uf = 'SC';
